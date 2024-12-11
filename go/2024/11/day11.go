abdee104a5facf74caa14a57077d58de664774c9
hugo.dartoy@telecomnancy.eu <hugo.dartoy@telecomnancy.eu>
2024-12-11 15:19:08 +0100

    [COMPLETE] 2024 Day11 + Tests

---
+++ go/2024/11/day11.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//go:embed input.txt
+var input string
+
+func parse(line string) map[int]int {
+	var numbers []string = strings.Split(line, " ")
+	var res map[int]int = make(map[int]int)
+	for _, number := range numbers {
+		var num, _ = strconv.Atoi(number)
+		res[num] += 1
+	}
+	return res
+}
+
+func countDigits(number int) (int, bool) {
+	var digits int = 1
+	var ok bool
+	var limit int = 10
+	for number >= limit {
+		digits++
+		limit *= 10
+		ok = !ok
+	}
+	return digits, ok
+}
+
+func split(number int, nbDigits int) (int, int) {
+	var n2 int
+	var length int = 1
+	for range nbDigits / 2 {
+		var lastDigit = number - 10*(number/10)
+		n2 += length * lastDigit
+		length *= 10
+		number = (number - lastDigit) / 10
+	}
+	return number, n2
+
+}
+
+func blink(val int) (int, int, bool) {
+	if val == 0 {
+		return 1, 0, false
+	}
+	var nbDigits, ok = countDigits(val)
+	if ok {
+		var n1, n2 = split(val, nbDigits)
+		return n1, n2, true
+	}
+	return val * 2024, 0, false
+}
+
+func sumMap(dict map[int]int) int {
+	var res int
+	for _, val := range dict {
+		res += val
+	}
+	return res
+}
+
+func solve(input string, loop int) int {
+	var lines = strings.Split(strings.TrimSuffix(input, "\n"), "\n")
+	var numbers map[int]int = parse(lines[0])
+	for range loop {
+		var newMap map[int]int = make(map[int]int)
+		for stone, number := range numbers {
+			var stone1, stone2, ok = blink(stone)
+			newMap[stone1] += number
+			if ok {
+				newMap[stone2] += number
+			}
+		}
+		numbers = newMap
+	}
+	return sumMap(numbers)
+}
+
+func part1(input string) int {
+	return solve(input, 25)
+}
+
+func part2(input string) int {
+	return solve(input, 75)
+}
+
+func main() {
+	fmt.Println("--2024 day 11 solution--")
+	start := time.Now()
+	fmt.Println("part1 : ", part1(input))
+	fmt.Println(time.Since(start))
+	start = time.Now()
+	fmt.Println("part2 : ", part2(input))
+	fmt.Println(time.Since(start))
+}
---
+++ go/2024/11/day11_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	_ "embed"
+	"maps"
+	"testing"
+)
+
+//go:embed input.txt
+var inputDay string
+
+func TestPart1Input(t *testing.T) {
+	result := part1(inputDay)
+	expected := 224529
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2Input(t *testing.T) {
+	result := part2(inputDay)
+	expected := 266820198587914
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestCountDigits(t *testing.T) {
+	var start = []int{0, 1, 9, 10, 50, 99, 100, 500, 10001, 1064842454}
+	var expected = []int{1, 1, 1, 2, 2, 2, 3, 3, 5, 10}
+	for i, val := range start {
+		var res, ok = countDigits(val)
+		if res != expected[i] || ok != (expected[i]%2 == 0) {
+			t.Errorf("Result is incorrect, got: %d,%t, want: %d,%t.", res, ok, expected[i], expected[i]%2 == 0)
+		}
+	}
+}
+
+func TestSplit(t *testing.T) {
+	var start = []int{10, 512072, 9664}
+	var expected1 = []int{1, 512, 96}
+	var expected2 = []int{0, 72, 64}
+	for i, val := range start {
+		var digits, _ = countDigits(val)
+		var n1, n2 = split(val, digits)
+		if n1 != expected1[i] || n2 != expected2[i] {
+			t.Errorf("Result is incorrect, got: %d,%d, want: %d,%d.", n1, n2, expected1[i], expected2[i])
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	var result = parse("48 56 123 560023 1")
+	var expected = map[int]int{48: 1, 56: 1, 123: 1, 560023: 1, 1: 1}
+	if !maps.Equal(result, expected) {
+		t.Errorf("Result is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestSumMap(t *testing.T) {
+	var result = sumMap(map[int]int{43: 2, 0: 98, 156: 29, 33: 1})
+	var expected = 130
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestBlinck(t *testing.T) {
+	var s11, _, ok1 = blink(0)
+	if ok1 || s11 != 1 {
+		t.Errorf("Result is incorrect, got: %d,%t, want: %d,%t.", s11, ok1, 1, false)
+	}
+	var s21, _, ok2 = blink(1)
+	if ok2 || s21 != 2024 {
+		t.Errorf("Result is incorrect, got: %d,%t, want: %d,%t.", s21, ok2, 2024, false)
+	}
+	var s31, s32, ok3 = blink(5568)
+	if !ok3 || s31 != 55 || s32 != 68 {
+		t.Errorf("Result is incorrect, got: %d,%d,%t, want: %d,%d,%t.", s31, s32, ok3, 55, 68, true)
+	}
+	var s41, s42, ok4 = blink(6948700036)
+	if !ok4 || s41 != 69487 || s42 != 36 {
+		t.Errorf("Result is incorrect, got: %d,%d,%t, want: %d,%d,%t.", s41, s42, ok4, 69487, 36, true)
+	}
+	var s51, _, ok5 = blink(8645321)
+	if ok5 || s51 != 2024*8645321 {
+		t.Errorf("Result is incorrect, got: %d,%t, want: %d,%t.", s51, ok5, 2024*8645321, false)
+	}
+}
