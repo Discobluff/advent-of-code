d26893d4c60217992cfebe949dce296d87c464a3
hugo.dartoy@telecomnancy.eu <hugo.dartoy@telecomnancy.eu>
2025-02-05 17:28:17 +0100

    test 2022 06

--- go/2022/06/day06_test.go
+++ go/2022/06/day06_test.go
@@ -12,7 +12,7 @@ func getInput(path string) string {
 }
 
 func TestPart1Input(t *testing.T) {
-	result := part1(getInput("../../../inputs/2022/06"))
+	result := part1(getInput("../../../inputs/2022/06/input.txt"))
 	expected := 1210
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
@@ -20,7 +20,7 @@ func TestPart1Input(t *testing.T) {
 }
 
 func TestPart2Input(t *testing.T) {
-	result := part2(getInput("../../../inputs/2022/06"))
+	result := part2(getInput("../../../inputs/2022/06/input.txt"))
 	expected := 3476
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
