03acf38d3345cb3c3122d7788ea926a6d1ef8af7
hugo.dartoy@telecomnancy.eu <hugo.dartoy@telecomnancy.eu>
2025-02-05 17:25:17 +0100

    2022 06 test

---
+++ go/2022/06/day06_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "embed"
+	"os"
+	"testing"
+)
+
+func getInput(path string) string {
+	data, _ := os.ReadFile(path)
+	return string(data)
+}
+
+func TestPart1Input(t *testing.T) {
+	result := part1(getInput("../../../inputs/2022/06"))
+	expected := 1210
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2Input(t *testing.T) {
+	result := part2(getInput("../../../inputs/2022/06"))
+	expected := 3476
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
