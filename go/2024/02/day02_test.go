4ed53983a9cb2f1fa13a11f45684feeb8ce1d3c7
hugo.dartoy@telecomnancy.eu <hugo.dartoy@telecomnancy.eu>
2025-02-05 11:14:38 +0100

    test 2024 02

--- go/2024/02/day02_test.go
+++ go/2024/02/day02_test.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	_ "embed"
+	"os"
 	"testing"
 )
 
-//go:embed input.txt
-var inputDay string
-
-//go:embed test.txt
-var input1 string
+func getInput(path string) string {
+	data, _ := os.ReadFile(path)
+	return string(data)
+}
 
 func TestPart1Input(t *testing.T) {
-	result := part1(inputDay)
+	result := part1(getInput("inputs/2024/02/input.txt"))
 	expected := 379
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
@@ -20,7 +20,7 @@ func TestPart1Input(t *testing.T) {
 }
 
 func TestPart1Input1(t *testing.T) {
-	result := part1(input1)
+	result := part1(getInput("inputs/2024/01/test.txt"))
 	expected := 2
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
@@ -28,7 +28,7 @@ func TestPart1Input1(t *testing.T) {
 }
 
 func TestPart2Input(t *testing.T) {
-	result := part2(inputDay)
+	result := part2(getInput("inputs/2024/02/input.txt"))
 	expected := 430
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
@@ -36,7 +36,7 @@ func TestPart2Input(t *testing.T) {
 }
 
 func TestPart2Input1(t *testing.T) {
-	result := part2(input1)
+	result := part2(getInput("inputs/2024/02/test.txt"))
 	expected := 4
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
