2bfd35cdb130dd2b3ff16003a08f3ff1667bae3d
hugo.dartoy@telecomnancy.eu <hugo.dartoy@telecomnancy.eu>
2025-02-05 15:27:20 +0100

    fix tests

--- go/2024/23/day23_test.go
+++ go/2024/23/day23_test.go
@@ -12,7 +12,7 @@ func getInput(path string) string {
 }
 
 func TestPart1Input(t *testing.T) {
-	result := part1(getInput("inputs/2024/23/input.txt"))
+	result := part1(getInput("../../../inputs/2024/23/input.txt"))
 	expected := 1352
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
@@ -20,7 +20,7 @@ func TestPart1Input(t *testing.T) {
 }
 
 func TestPart2Input(t *testing.T) {
-	result := part2(getInput("inputs/2024/23/input.txt"))
+	result := part2(getInput("../../../inputs/2024/23/input.txt"))
 	expected := "dm,do,fr,gf,gh,gy,iq,jb,kt,on,rg,xf,ze"
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %s, want: %s.", result, expected)
